bed1a74b2fd1a0f3a1ad4d41fb746e2b02d7fd58
thedoomfan2004 <119742495+thedoomfan2004@users.noreply.github.com>
2022-12-03 19:33:33 +0300

    Add files via upload

---
+++ кибербуллинг/app.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.variants', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
